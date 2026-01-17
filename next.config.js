03bef115cc45ebf5e8906dc7db4c8f8e70b876ed
Abd-El Rahman Taha <b0ody66taha@gmail.com>
2026-01-17 19:11:37 +0200

    Initial commit - Project Finished

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // ضيف السطر ده هنا
+  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei', 'troika-three-text'],
+}
 
 module.exports = nextConfig
